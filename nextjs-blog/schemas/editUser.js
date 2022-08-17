5e256fcf9b7aecf22a3484b7e7be3001d9da5237
Peter Yoo <trustintofu@gmail.com>
2022-08-17 13:35:22 -0700

    wip: installs Sanity backend, adds schemas for blog,user, socials

---
+++ nextjs-blog/schemas/editUser.js
@@ -0,0 +1,53 @@
+export default {
+  name: 'editUser',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'contactEmail',
+      title: 'Contact Email',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'socialsTitle',
+      title: 'Socials Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'showSocialsSection',
+      title: 'Social Section',
+      type: 'boolean',
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+  preview: {
+    select: {
+      name: 'image',
+      media: 'image',
+      title: 'name',
+    },
+  },
+};
