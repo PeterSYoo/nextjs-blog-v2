7e67b4efb892bec73108221fea596c84f31cea2b
Peter Yoo <trustintofu@gmail.com>
2022-08-18 21:02:33 -0700

    wip: refactors page for mobile 1st approach, adds hamburger menu for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,121 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
+import { Transition } from '@headlessui/react';
 import { IoHomeSharp, IoBriefcase, IoMailSharp } from 'react-icons/io5';
 import { SiGithub, SiLinkedin } from 'react-icons/si';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Home: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <Head>
         <title>Peter Yoo's Blog</title>
       </Head>
 
-      <main>
-        <div className="grid overflow-hidden grid-cols-4 grid-rows-2 h-screen">
-          <div className="row-span-2 p-5 flex flex-col border-r-gray-100 border-r-2 py-20">
-            <div className="flex justify-end text-4xl font-bold">
-              Hi!&nbsp;<section className="text-4xl">👋</section>
+      <header>
+        <nav className="p-5 bg-white border-b md:flex md:items-center md:justify-between">
+          <div className="flex justify-between items-center">
+            <div className="text-2xl font-bold">
+              Hi! <span>👋</span>
+              <div className="text-2xl font-bold">I'm Peter</div>
             </div>
-            <div className="flex justify-end text-4xl font-bold">I'm Peter</div>
-            <div className="">
-              <ul className="list-none flex flex-col items-end gap-20 py-20 font-serif text-2xl">
-                <li className="cursor-pointer">
-                  <IoHomeSharp />
-                </li>
-                <li className="cursor-pointer">
-                  <IoBriefcase />
-                </li>
-                <li className="cursor-pointer">
-                  <SiGithub />
-                </li>
-                <li className="cursor-pointer">
-                  <SiLinkedin />
-                </li>
-                <li className="cursor-pointer">
-                  <IoMailSharp />
-                </li>
-              </ul>
-            </div>
+
+            <span className="text-3xl cursor-pointer md:hidden block">
+              <button
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+              >
+                {!isOpen ? <GiHamburgerMenu /> : <AiOutlineClose />}
+              </button>
+            </span>
           </div>
-          <div className="row-span-2 col-span-3 py-40 px-20 flex justify-center">
-            <div className="">
-              <div className="">Post Component</div>
-            </div>
-          </div>
-        </div>
-      </main>
+
+          <Transition
+            show={isOpen}
+            enter="transition ease-out duration-100 transform"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition ease-in duration-75 transform"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            {(ref) => (
+              // handle the mobile menu tabs
+              <div className="md:hidden id=mobile-menu flex justify-end">
+                <div className="px-2 pt-2 pb-3 space-y-1 sml:px-3" ref={ref}>
+                  <Link
+                    href="/"
+                    activeClass="home"
+                    to="home"
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    className="cursor-pointer"
+                  >
+                    Home
+                  </Link>
+                </div>
+              </div>
+            )}
+          </Transition>
+
+          <ul className="text-right md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+            <li className="mx-4 my-6 md:my-0">
+              <Link href="/">
+                <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
+                  HOME
+                </span>
+              </Link>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <Link href="https://petersyoo.netlify.app/" passHref>
+                <a target="_blank" rel="noopener noreferrer">
+                  <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
+                    PORTFOLIO
+                  </span>
+                </a>
+              </Link>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <Link href="https://github.com/PeterSYoo">
+                <a target="_blank" rel="noopener noreferrer">
+                  <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
+                    GITHUB
+                  </span>
+                </a>
+              </Link>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <Link href="https://www.linkedin.com/in/petersyoo/">
+                <a target="_blank" rel="noopener noreferrer">
+                  <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
+                    LINKEDIN
+                  </span>
+                </a>
+              </Link>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <Link href="mailto:petersyoox@gmail.com">
+                <a target="_blank" rel="noopener noreferrer">
+                  <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
+                    EMAIL
+                  </span>
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main></main>
     </>
   );
 };
