7d45b65dd58bf9b0b544cc60eb4dd88d1f18bb52
Peter Yoo <trustintofu@gmail.com>
2022-08-19 20:26:06 -0700

    wip: adds 2 col grid layout to non mobile layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,102 +18,161 @@ const Home: NextPage = () => {
         <title>Peter Yoo's Blog</title>
       </Head>
 
-      <header>
-        <nav className="p-5 bg-white border-b md:flex md:items-center md:justify-between">
-          <div className="flex justify-between items-center">
-            <div className="text-2xl font-bold">
-              Hi! <span>👋</span>
-              <div className="text-2xl font-bold">I'm Peter</div>
-            </div>
-
-            <span className="text-3xl cursor-pointer md:hidden block">
-              <button
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-              >
-                {!isOpen ? <GiHamburgerMenu /> : <AiOutlineClose />}
-              </button>
-            </span>
+      <div className="p-5 px-10 xl:px-20 max-w-screen-2xl md:p-0 md:px-0 bg-white md:border-none md:grid md:m-auto md:grid-rows-1 md:grid-cols-4 md:w-auto md:h-screen">
+        <div className="flex justify-between items-center md:flex-col md:items-end md:text-right md:justify-start md:p-5 md:pt-20 md:box md:col-span-1 md:row-span-2 md:border-r">
+          <div className="text-2xl md:text-4xl font-bold">
+            Hi! <span>👋</span>
+            <div className="text-2xl md:text-4xl font-bold">I'm Peter</div>
+          </div>
+          <div className="invisible md:flex md:flex-col md:items-end md:gap-20 md:visible md:pt-20">
+            <Link
+              href="/"
+              activeClass="home"
+              to="home"
+              className="cursor-pointer"
+            >
+              <IoHomeSharp className="text-3xl cursor-pointer" />
+            </Link>
+            <Link
+              href="https://petersyoo.netlify.app/"
+              activeClass="portfolio"
+              to="portfolio"
+              className="cursor-pointer"
+              passHref
+            >
+              <a target="_blank">
+                <IoBriefcase className="text-3xl cursor-pointer" />
+              </a>
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/petersyoo/"
+              activeClass="linkedin"
+              to="linkedin"
+              className="cursor-pointer"
+              passHref
+            >
+              <a target="_blank">
+                <SiLinkedin className="text-3xl cursor-pointer" />
+              </a>
+            </Link>
+            <Link
+              href="https://github.com/PeterSYoo"
+              activeClass="github"
+              to="github"
+              className="cursor-pointer"
+              passHref
+            >
+              <a target="_blank">
+                {' '}
+                <SiGithub className="text-3xl cursor-pointer" />
+              </a>
+            </Link>
+            <Link
+              href="mailto:petersyoox@gmail.com"
+              activeClass="email"
+              to="email"
+              className="cursor-pointer"
+              passHref
+            >
+              <a target="_blank">
+                <IoMailSharp className="text-3xl cursor-pointer" />
+              </a>
+            </Link>
           </div>
 
-          <Transition
-            show={isOpen}
-            enter="transition ease-out duration-100 transform"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="transition ease-in duration-75 transform"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            {(ref) => (
-              // handle the mobile menu tabs
-              <div className="md:hidden id=mobile-menu flex justify-end">
-                <div className="px-2 pt-2 pb-3 space-y-1 sml:px-3" ref={ref}>
-                  <Link
-                    href="/"
-                    activeClass="home"
-                    to="home"
-                    smooth={true}
-                    offset={50}
-                    duration={500}
-                    className="cursor-pointer"
-                  >
-                    Home
-                  </Link>
-                </div>
-              </div>
-            )}
-          </Transition>
+          <span className="text-3xl cursor-pointer md:hidden block">
+            <button
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+            >
+              {!isOpen ? <GiHamburgerMenu /> : <AiOutlineClose />}
+            </button>
+          </span>
+        </div>
 
-          <ul className="text-right md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
-            <li className="mx-4 my-6 md:my-0">
-              <Link href="/">
-                <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
-                  HOME
-                </span>
-              </Link>
-            </li>
-            <li className="mx-4 my-6 md:my-0">
-              <Link href="https://petersyoo.netlify.app/" passHref>
-                <a target="_blank" rel="noopener noreferrer">
-                  <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
-                    PORTFOLIO
-                  </span>
-                </a>
-              </Link>
-            </li>
-            <li className="mx-4 my-6 md:my-0">
-              <Link href="https://github.com/PeterSYoo">
-                <a target="_blank" rel="noopener noreferrer">
-                  <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
-                    GITHUB
-                  </span>
-                </a>
-              </Link>
-            </li>
-            <li className="mx-4 my-6 md:my-0">
-              <Link href="https://www.linkedin.com/in/petersyoo/">
-                <a target="_blank" rel="noopener noreferrer">
-                  <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
-                    LINKEDIN
-                  </span>
-                </a>
-              </Link>
-            </li>
-            <li className="mx-4 my-6 md:my-0">
-              <Link href="mailto:petersyoox@gmail.com">
-                <a target="_blank" rel="noopener noreferrer">
-                  <span className="text-2xl cursor-pointer hover:text-gray-200 duration-500">
-                    EMAIL
-                  </span>
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100 md:hidden"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100 md:hidden"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            // handle the mobile menu tabs
+            <div className="md:hidden id=mobile-menu flex justify-end">
+              <div className="flex border-b w-full bg-white flex-col text-right text-2xl px-2 pb-10 space-y-4 sml:px-3">
+                <Link
+                  href="/"
+                  activeClass="home"
+                  to="home"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  className="cursor-pointer"
+                >
+                  Home
+                </Link>
+                <Link
+                  href="https://petersyoo.netlify.app/"
+                  activeClass="portfolio"
+                  to="portfolio"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  className="cursor-pointer"
+                  passHref
+                >
+                  <a target="_blank">Portfolio</a>
+                </Link>
+                <Link
+                  href="https://www.linkedin.com/in/petersyoo/"
+                  activeClass="linkedin"
+                  to="linkedin"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  className="cursor-pointer"
+                  passHref
+                >
+                  <a target="_blank">LinkedIn</a>
+                </Link>
+                <Link
+                  href="https://github.com/PeterSYoo"
+                  activeClass="github"
+                  to="github"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  className="cursor-pointer"
+                  passHref
+                >
+                  <a target="_blank">GitHub</a>
+                </Link>
+                <Link
+                  href="mailto:petersyoox@gmail.com"
+                  activeClass="email"
+                  to="email"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  className="cursor-pointer"
+                  passHref
+                >
+                  <a target="_blank">Email</a>
+                </Link>
+              </div>
+            </div>
+          )}
+        </Transition>
+        <div className="pt-10 flex flex-col md:p-20 md:box md:row-span-1 md:col-span-3">
+          Content
+          <div className="border-b border-gr-400"></div>
+        </div>
+      </div>
 
       <main></main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1600px',
+        '4xl': '1800px',
+        '5xl': '2000px',
+        '6xl': '2200px',
+        '7xl': '2400px',
+        '8xl': '2550px',
+      },
+      padding: {
+        '10em': '10em',
+      },
+    },
   },
   plugins: [],
-}
+};
